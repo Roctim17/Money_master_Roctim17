15eaeabf1ca071f6262c61dd0d7373538fd5e27b
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-02-17 09:50:58 +0600

    add calculate

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+
 // Total Expenses
-const food = document.getElementById('food').value;
-const rent = document.getElementById('rent').value;
-const clothes = document.getElementById('clothes').value;
-function expenses(food, rent, clothes) {
+
+function expenses() {
+    const food = parseInt(document.getElementById('food').value)
+    const rent = parseInt(document.getElementById('rent').value)
+    const clothes = parseInt(document.getElementById('clothes').value)
+
     totalExpenses = food + rent + clothes;
     return totalExpenses;
 }
-console.log(expenses(food, rent, clothes))
+// calculate click function
+document.getElementById('calculate').addEventListener("click", function () {
+    const income = parseInt(document.getElementById('income').value)
+    const netIncome = income - expenses();
+    document.getElementById('total-expenses').innerText = expenses();
+    document.getElementById('balance').innerText = netIncome;
+
+
+    console.log("expenses(food, rent, clothes)");
+    console.log(income);
+    console.log(expenses());
+    console.log(netIncome);
+
+
+})
+// save click function
+
