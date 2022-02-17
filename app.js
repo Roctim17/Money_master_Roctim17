d58167b5856956e438686ef1fa4e3ec645c95757
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-02-17 13:24:56 +0600

    error massage

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // income
 function income() {
-    const totalIncome = parseInt(document.getElementById('income').value)
+    const totalIncome = document.getElementById('income').value;
+    if (totalIncome > 0) {
+        document.getElementById('error-Minusnumber').style.display = "none";
+        document.getElementById('error-letter').style.display = "none";
+        document.getElementById('error-text').style.display = "none";
+    }
+    else if (totalIncome < 0) {
+        document.getElementById('error-Minusnumber').style.display = "block";
+        document.getElementById('error-letter').style.display = "none";
+        document.getElementById('error-text').style.display = "none";
+    }
+    else if (totalIncome != "number") {
+        document.getElementById('error-letter').style.display = "block";
+        document.getElementById('error-text').style.display = "none";
+        document.getElementById('error-Minusnumber').style.display = "none";
+    }
+    else {
+        document.getElementById('error-text').style.display = "block";
+    }
     return totalIncome
 }
 // Total Expenses
@@ -9,17 +27,26 @@ function expenses() {
     const rent = parseInt(document.getElementById('rent').value)
     const clothes = parseInt(document.getElementById('clothes').value)
     totalExpenses = food + rent + clothes;
+
     return totalExpenses;
 }
 // Balance
 function netIncome() {
     const balance = income() - expenses();
+    if (income() > expenses()) {
+
+    }
+    else {
+        document.getElementById('error-balance').style.display = "block";
+    }
     return balance;
 }
+
 // calculate click function
 document.getElementById('calculate').addEventListener("click", function () {
     document.getElementById('total-expenses').innerText = expenses();
     document.getElementById('balance').innerText = netIncome();
+    console.log(income());
 })
 // save click function
 document.getElementById('save').addEventListener("click", function () {
